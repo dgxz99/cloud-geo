0a28c04556fb1c27fa2698a9f734e7666e921ad2
大锅 <1944182256@qq.com>
2024-06-06 17:07:38 +0800

    pywps注册到consul

---
+++ pywps-pyqgis/consul_service.py
@@ -0,0 +1,38 @@
+import configparser
+import os
+from consul import Consul
+
+config_path = os.path.join(os.path.dirname(__file__), 'pywps.cfg')
+config = configparser.ConfigParser()
+config.read(config_path)
+
+# Consul地址和端口
+consul_host = config.get("consul", "consul_ip")
+consul_port = config.getint("consul", "consul_port")
+
+# 全局唯一Consul客户端对象
+consul_client = Consul(host=consul_host, port=consul_port)
+
+
+# 注册服务
+def register_consul(service_name, service_address, service_port):
+	# 服务ID，每个节点应该唯一
+	service_id = f'{service_name}-{service_address}-{service_port}'
+	# 注册服务
+	consul_client.agent.service.register(
+		service_id=service_id,
+		name=service_name,
+		address=service_address,
+		port=service_port,
+		tags=['dev'],
+		check={
+			'http': f'http://{service_address}:{service_port}/health',
+			'interval': '10s',
+			'timeout': '1s',
+		})
+
+
+# 注销节点
+def deregister_consul(service_name, service_address, service_port):
+	service_id = f'{service_name}-{service_address}-{service_port}'
+	consul_client.agent.service.deregister(service_id)
--- pywps-pyqgis/main.py
+++ pywps-pyqgis/main.py
@@ -1,3 +1,5 @@
+import atexit
+import configparser
 import json
 import os
 import random
@@ -9,6 +11,10 @@ from algorithm_init.mongo import MongoDB
 from pywps import Service, configuration
 from qgis.core import QgsApplication
 from processes.QGISProFactory import QGISProcFactory
+
+config_path = os.path.join(os.path.dirname(__file__), 'pywps.cfg')
+config = configparser.ConfigParser()
+config.read(config_path)
 
 app = flask.Flask(__name__)
 # 算子初始化
@@ -33,41 +39,7 @@ def hello():
 
 @app.route('/<base_url>', methods=['GET', 'POST'])
 def wps_handle(base_url):
-	"""
-		This function parses the request URL and extracts the following:
-			default operation, process identifier, output_ids, default mimetype
-			info that cannot be terminated from the URL will be None (default)
-
-			The url is expected to be in the following format, all the levels are optional.
-			[base_url]/[identifier]/[output_ids]
-
-		:return: dict with the extracted info listed:
-			base_url - [wps|processes|jobs|api/api_level]
-			default_mimetype - determinate by the base_url part:
-				XML - if the base url == 'wps',
-				JSON - if the base URL in ['api'|'jobs'|'processes']
-			operation - also determinate by the base_url part:
-				['api'|'jobs'] -> 'execute'
-				processes -> 'describeProcess' or 'getCapabilities'
-					'describeProcess' if identifier is present as the next item, 'getCapabilities' otherwise
-			api - api level, only expected if base_url=='api'
-			identifier - the process identifier
-			output_ids - if exist then it selects raw output with the name output_ids
-	"""
 	return service
-
-
-# @app.route('/api/alg', methods=['POST'])
-# def test_pyqgs_alg():
-# 	# 获取请求中的JSON数据
-# 	# data = request.get_json()
-# 	inp = [r"C:\Users\admin\Desktop\PyWPS\算子实验数据\gdal_merge\INPUT\srtm_58_05.tif", r"C:\Users\admin\Desktop\PyWPS\算子实验数据\gdal_merge\INPUT\srtm_59_05.tif"]
-# 	outp = rf'C:\Users\admin\Desktop\PyWPS\data\temp\{uuid.uuid4()}.tif'
-# 	try:
-# 		processing.run("gdal:merge", {'INPUT': inp, 'OUTPUT': outp})
-# 		return jsonify({"success": "run success!"}), 200
-# 	except:
-# 		return jsonify({"error": "run error!"}), 400
 
 
 @app.route('/outputs/' + '<path:filename>')
@@ -79,7 +51,6 @@ def outputfile(filename):
 			mime_type = 'text/xml'
 		else:
 			mime_type = 'application/octet-stream'
-		# return flask.Response(file_bytes, content_type=mime_type)
 		# 设置响应头，告诉浏览器要下载文件，且适合下载大文件
 		response = flask.send_file(target_file, mimetype=mime_type)
 		response.headers["Content-Disposition"] = f"attachment; filename={filename}"
@@ -101,12 +72,19 @@ def static_file(filename):
 		flask.abort(404)
 
 
-@app.route('/description/<path:identifier>', methods=['GET'])
+@app.route('/processes/<path:identifier>', methods=['GET'])
 def describe_process(identifier):
 	alg = MongoDB().find_one("algorithms", {"Identifier": identifier})
 	if alg and '_id' in alg:
 		alg['_id'] = str(alg['_id'])
 	return jsonify(alg)
+
+
+@app.route('/health')
+def health_check():
+	# 在这里执行健康检查逻辑，例如检查数据库连接、依赖服务等
+	# 如果一切正常，返回HTTP 200状态码和一个表示健康的响应
+	return jsonify({"status": "healthy"}), 200
 
 
 @app.route('/list-algorithms', methods=['GET'])
@@ -144,41 +122,14 @@ def publish_features():
 	return layer_name
 
 
+from consul_service import register_consul, deregister_consul
+
+service_name = config.get("consul", "service_name")
+service_ip = config.get("consul", "service_ip")
+service_port = config.getint("consul", "service_port")
+
+register_consul(service_name, service_ip, service_port)
+atexit.register(deregister_consul, service_name, service_ip, service_port)
+
 if __name__ == "__main__":
 	app.run()
-# import argparse
-#
-# parser = argparse.ArgumentParser(
-# 	description="""Script for starting an example PyWPS instance with sample processes""",
-# 	epilog="""Do not use this service in a production environment.
-# 	It's intended to be running in test environment only!
-# 	For more documentation, visit http://pywps.org/doc
-# 	"""
-# )
-# parser.add_argument('-d', '--daemon', action='store_true', help="run in daemon mode")
-# parser.add_argument(
-# 	'-a',
-# 	'--all-addresses',
-# 	action='store_true',
-# 	help="run flask using IPv4 0.0.0.0 (all network interfaces), otherwise bind to 127.0.0.1 (localhost). This maybe necessary in systems that only run Flask")
-# args = parser.parse_args()
-#
-# if args.all_addresses:
-# 	bind_host = '0.0.0.0'
-# else:
-# 	bind_host = '127.0.0.1'
-#
-# if args.daemon:
-# 	pid = None
-# 	try:
-# 		pid = os.fork()
-# 	except OSError as e:
-# 		raise Exception("%s [%d]" % (e.strerror, e.errno))
-#
-# 	if pid == 0:
-# 		os.setsid()
-# 		app.run(threaded=True, host=bind_host)
-# 	else:
-# 		os._exit(0)
-# else:
-# 	app.run(threaded=True, host=bind_host)
