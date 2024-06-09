ca6e1c2a5b8fddda8773f8d54dcd551e799e1403
大锅 <1944182256@qq.com>
2024-06-09 18:27:29 +0800

    基本调试异步功能完成

--- pywps-pyqgis/src/api/pywpsAPI.py
+++ pywps-pyqgis/src/api/pywpsAPI.py
@@ -1,6 +1,11 @@
 import json
 import random
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import uuid
+
 import flask
+import requests
 
 from dao.mongo import MongoDB
 from pywps import Service, configuration
@@ -21,6 +26,9 @@ service = Service(processes, ['pywps.cfg'])
 
 pywps_blue = flask.Blueprint('pywps', __name__)
 
+# 创建线程池
+executor = ThreadPoolExecutor(max_workers=4)
+
 
 @pywps_blue.route("/")
 def hello():
@@ -29,9 +37,50 @@ def hello():
 	return flask.render_template('home.html', request_url=request_url, server_url=server_url, process_descriptor=process_descriptor)
 
 
+# 存储任务状态和结果
+job_status = {}
+job_results = {}
+
+
+def run_job(req, job_id):
+	try:
+		print(req)
+
+		res = requests.post(req["req_base_url"], json=req["req_data"])
+		job_status[job_id] = "Succeeded"
+		job_results[job_id] = res
+	except Exception as e:
+		job_status[job_id] = f"Failed: {str(e)}"
+		job_results[job_id] = None
+
+
 @pywps_blue.route('/<base_url>', methods=['GET', 'POST'])
 def wps_handle(base_url):
-	return service
+	flask_request = flask.request
+	data = json.loads(flask_request.data)
+	mode = data.get("mode", None)
+	if mode:
+		del data["mode"]
+	job_id = str(uuid.uuid4())
+
+	req = {
+		"req_data": data,
+		"req_method": flask_request.method,
+		"req_base_url": "http://127.0.0.1:5000/jobs",
+	}
+
+	if mode == "async":
+		job_status[job_id] = "Running"
+		executor.submit(run_job, req, job_id)
+		return flask.jsonify({'job-id': job_id})
+
+	return service.call(flask_request)
+
+
+@pywps_blue.route('/jobs/<job_id>', methods=['GET'])
+def get_job_status(job_id):
+	status = job_status.get(job_id, "Unknown job ID")
+	return flask.jsonify({'job-id': job_id, 'status': status})
 
 
 @pywps_blue.route('/processes/<path:identifier>', methods=['GET'])
--- pywps-pyqgis/src/main.py
+++ pywps-pyqgis/src/main.py
@@ -1,13 +1,10 @@
 import atexit
-import configparser
-import os
 import flask
 from utils.consul_service import register_consul, deregister_consul
 from api.pywpsAPI import pywps_blue
 from context.config import get_config
 
 config = get_config()
-
 
 # 获取consul相关配置
 service_name = config.get("consul", "service_name")
@@ -20,6 +17,7 @@ atexit.register(deregister_consul, service_name, service_ip, service_port)
 
 app = flask.Flask(__name__)
 
+# 注册pywps_blue
 app.register_blueprint(pywps_blue, url_prefix='')
 
 if __name__ == "__main__":
