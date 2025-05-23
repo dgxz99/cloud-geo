ea3a9756fb822746520ca18ae438caa0f9ae4c38
大锅 <1944182256@qq.com>
2025-05-23 21:10:11 +0800

    添加前端dockerfile文件

---
+++ dockerfile/web-nginx/build.fish
@@ -0,0 +1,28 @@
+#!/bin/fish
+
+# 接收版本号参数
+set image_version $argv[1]
+
+if test -z $image_version
+    echo 请指定版本号！
+    exit
+end
+
+# 打包代码
+echo 正在打包代码...
+cd ../../PyWPS-fronted
+npm run build
+tar -cvf ../dockerfile/web-nginx/frontend.tar -C ./dist/ .
+
+# 构建Docker镜像
+set image_name swsk33/cloud-geo-nginx
+echo 正在构建镜像...
+docker build --build-arg ALL_PROXY="http://host.docker.internal:7500" -f Dockerfile -t $image_name:$image_version .
+echo 创建latest tag...
+docker tag $image_name:$image_version $image_name
+
+# 清理
+echo 清理打包...
+rm *.tar
+
+echo 构建完成！
