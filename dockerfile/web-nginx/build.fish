612687cd1a28c0b8c0f694d8acef2df0731f987d
大锅 <1944182256@qq.com>
2025-05-25 17:01:15 +0800

    前端Nginx镜像设置两个版本

--- dockerfile/web-nginx/build.fish
+++ dockerfile/web-nginx/build.fish
@@ -18,9 +18,15 @@ tar -cvf ../dockerfile/web-nginx/frontend.tar -C ./dist/ .
 set image_name swsk33/cloud-geo-nginx
 echo 正在构建镜像...
 cd ../dockerfile/web-nginx/
-docker build --build-arg ALL_PROXY="http://host.docker.internal:7500" -f Dockerfile -t $image_name:$image_version .
-echo 创建latest tag...
-docker tag $image_name:$image_version $image_name
+
+# 构建分布式部署用的镜像
+echo "Building distributed image..."
+docker build --build-arg ALL_PROXY="http://host.docker.internal:7500" -f Dockerfile-distributed -t $image_name:distributed-$image_version .
+
+
+# 构建单体部署用的镜像
+echo "Building single image..."
+docker build --build-arg ALL_PROXY="http://host.docker.internal:7500" -f Dockerfile-single -t $image_name:single-$image_version .
 
 # 清理
 echo 清理打包...
